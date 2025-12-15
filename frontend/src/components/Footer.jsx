6b4d7cd048d5b8b32451968b8f0477022566dcd0
venom-cooder <pavitradurgeshpandey@gmail.com>
2025-12-16 02:12:47 +0530

    logos are being updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,6 +8,16 @@ const Footer = () => {
 
   return (
     <footer>
+      {/* Internal Style for Icon Animation */}
+      <style>{`
+        .social-icon-anim {
+          transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        }
+        .social-icon-anim:hover {
+          transform: scale(1.3);
+        }
+      `}</style>
+
       <div className="footer-grid">
         {/* Products */}
         <div className="footer-col">
@@ -52,11 +62,29 @@ const Footer = () => {
             ab.customlabels@gmail.com <FaEnvelope size={14} color="#888"/>
           </div>
           
-          {/* Social Icons Row */}
-          <div className="social-row">
-            <FaInstagram size={24} cursor="pointer" color="#E1306C" onClick={()=>openLink('https://www.instagram.com/abcustomlables')} />
-            <FaWhatsapp size={24} cursor="pointer" color="#25D366" onClick={()=>openLink('https://wa.me/919243858944')} />
-            <FaFacebook size={24} cursor="pointer" color="#1877F2" onClick={()=>openLink('https://www.facebook.com/profile.php?id=61563586884230&rdid=GllYRaArSfdJjcxH&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1DU7qtJUbN%2F')} />
+          {/* UPDATED SOCIAL ROW: Bigger Icons, More Gap, Animation */}
+          <div style={{ display: 'flex', gap: '25px', marginTop: '20px', alignItems: 'center' }}>
+            <FaInstagram 
+              size={32} 
+              className="social-icon-anim" 
+              cursor="pointer" 
+              color="#E1306C" 
+              onClick={()=>openLink('https://www.instagram.com/abcustomlables')} 
+            />
+            <FaWhatsapp 
+              size={32} 
+              className="social-icon-anim" 
+              cursor="pointer" 
+              color="#25D366" 
+              onClick={()=>openLink('https://wa.me/919243858944')} 
+            />
+            <FaFacebook 
+              size={32} 
+              className="social-icon-anim" 
+              cursor="pointer" 
+              color="#1877F2" 
+              onClick={()=>openLink('https://www.facebook.com/profile.php?id=61563586884230&rdid=GllYRaArSfdJjcxH&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1DU7qtJUbN%2F')} 
+            />
           </div>
         </div>
       </div>
